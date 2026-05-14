aee4cf9fb34fbfd4d9347c39b68fac6e20b4a811
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-14 11:13:14 -0300

    fix: arrumando css e js

--- src/js/script.js
+++ src/js/script.js
@@ -12,11 +12,12 @@ async function configurarCamera(){
             //habilita a camera traseira do celular
             video:{facingMode:"environment"},
             audio:false
-        })
+        });
         //atribui o fluxo da camera ao elemento de video para visualizar
         videoElemento.srcObject = midia;
+        videoElemento.play();
     }catch(erro){
-        resultado.innerText="Erro ao acessar a câmera",erro;
+        resultado.innerText="Erro ao acessar a câmera" + erro.message;
     }
 }
 //executa a função para habilitar camera
@@ -36,6 +37,10 @@ botaoScanear.onclick = async()=>{
     canvas.widht = videoElemento.videoWidht;
     canvas.height = videoElemento.videoHeight;
 
+    contexto.setTransform(1,0,0,1,0,0);
+
+    contexto.filter='contrast(1.2) grayscale(1)';
+
     //desenha o frame atual do video dentreo do canvas(tira a foto)
     contexto.drawImage(videoElemento,0,0,canvas,canvas.height);
 
@@ -44,17 +49,17 @@ botaoScanear.onclick = async()=>{
         //função do tesserect
         const {data:{text}}= await Tesseract.recognize(
             canvas, //a imagem que acabou de capturar
-            'por', // idioma em portugues
-            {looger: m=>console.log(m)} //mostra no log
-        )
-        resultado.innerText = text.trim().length > 0 ? text : "Não foi possivel identificar o texto";
+            'por' // idioma em portugues
+            
+        );
+         resultado.innerText = textoFinal.length > 0 ? textoFinal : "Não foi possível identificar o texto";
 
     }catch(erro){
         //resultado caso apresente um erro
-        resultado.innerText="Erro no processamento",erro.message;
+        resultado.innerText="Erro no processamento" + erro.message;
     }
     finally{
         //habilita o botão para uma nova leitura
         botaoScanear.disabled=false;
     }
-}
+};
